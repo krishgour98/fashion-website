887bef6cf62356239816cdf55c2d5a3fe5a173b2
krishgour98 <gourkrish985@gmail.com>
2025-12-22 12:44:29 +0530

    Updates

--- Badge.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Badge = ({ children, className = '' }) => {
-  return (
-    <span className={`inline-block px-2 py-1 rounded-full text-xs font-bold ${className}`}>{children}</span>
-  );
-};
-
-export default Badge;
